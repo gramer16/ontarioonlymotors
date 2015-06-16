d901cae3f2afced758a4e0fb989426b242d0fae9
graciem <gramer16@dnginternational.ca>
2015-06-16 18:46:17 -0400

    D

---
+++ db/migrate/20150616224504_change_zipcode_in_accountants.rb
@@ -0,0 +1,9 @@
+class ChangeZipcodeInAccountants < ActiveRecord::Migration
+  def self.up
+    change_column :accountants, :zipcode, :string
+  end
+ 
+  def self.down
+    change_column :accountants, :zipcode, :integer
+  end
+end
